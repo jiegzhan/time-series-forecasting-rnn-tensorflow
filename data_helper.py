6dc65227a2146ce5e612b0fde40397ba9bff149c
Jie Zhang <jiegzhan@gmail.com>
2018-01-27 01:39:02 +0000

    data helper

---
+++ data_helper.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+# create a differenced series
+def difference(dataset, interval=1):
+	diff = list()
+	for i in range(interval, len(dataset)):
+		value = dataset[i] - dataset[i - interval]
+		diff.append(value)
+	return pd.Series(diff)
+
+# invert differenced value
+def inverse_difference(history, yhat, interval=1):
+	return yhat + history[-interval]
+
+# frame a sequence as a supervised learning problem
+def timeseries_to_supervised(data, lag=1):
+	df = pd.DataFrame(data)
+	columns = [df.shift(i) for i in range(1, lag+1)]
+	columns.append(df)
+	df = pd.concat(columns, axis=1)
+	df.fillna(0, inplace=True)
+	return df
