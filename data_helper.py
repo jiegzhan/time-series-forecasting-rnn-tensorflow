66825ee7a643af41fa9306a0cae1ada738f01a75
Jie Zhang <jiegzhan@gmail.com>
2018-01-29 19:15:54 -0800

    Predict the next value

--- data_helper.py
+++ data_helper.py
@@ -2,34 +2,46 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def load_timeseries(filename, seq_len, normalise_window):
+def load_timeseries(filename, window_size):
 	"""Load time series dataset"""
 
 	series = pd.read_csv(filename, sep=',', header=0, index_col=0, squeeze=True)
 	data = series.values
 
-	sequence_length = seq_len + 1
-	result = []
+	sequence_length = window_size + 1
+
+	raw = []
 	for index in range(len(data) - sequence_length):
-		result.append(data[index: index + sequence_length])
+		raw.append(data[index: index + sequence_length])
 
-	if normalise_window:
-		result = normalise_windows(result)
+	result = normalise_windows(raw)
 
+	raw = np.array(raw)
 	result = np.array(result)
 
-	row = round(0.8 * result.shape[0])
-	train = result[:int(row), :]
+	split_ratio = round(0.8 * result.shape[0])
+	train = result[:int(split_ratio), :]
 	np.random.shuffle(train)
+
 	x_train = train[:, :-1]
 	y_train = train[:, -1]
-	x_test = result[int(row):, :-1]
-	y_test = result[int(row):, -1]
+
+	x_test = result[int(split_ratio):, :-1]
+	y_test = result[int(split_ratio):, -1]
 
 	x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 	x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))  
 
-	return [x_train, y_train, x_test, y_test]
+	x_test_raw = raw[int(split_ratio):, :-1]
+	y_test_raw = raw[int(split_ratio):, -1]
+
+	# Last window, for next time stamp prediction
+	last_raw = [data[-window_size:]]
+	last = normalise_windows(last_raw)
+	last = np.array(last)
+	last = np.reshape(last, (last.shape[0], last.shape[1], 1))
+
+	return [x_train, y_train, x_test, y_test, x_test_raw, y_test_raw, last_raw, last]
 
 def normalise_windows(window_data):
 	"""Normalize data"""
@@ -40,12 +52,17 @@ def normalise_windows(window_data):
 		normalised_data.append(normalised_window)
 	return normalised_data
 
-def plot_results(predicted_data, true_data):
+def plot_results(predicted_data, true_data, predicted_raw, true_data_raw):
 	"""Plot predictions VS true data"""
 
 	fig = plt.figure(facecolor='white')
-	ax = fig.add_subplot(111)
+	ax = fig.add_subplot(221)
 	ax.plot(true_data, label='True Data')
 	plt.plot(predicted_data, label='Prediction')
+	plt.legend()
+
+	plt.subplot(223)
+	plt.plot(true_data_raw, label='true_data_raw')
+	plt.plot(predicted_raw, label='predicted_raw')	
 	plt.legend()
 	plt.show()
--- train_predict.py
+++ train_predict.py
@@ -16,7 +16,7 @@ def train_predict():
 	params = json.loads(open(parameter_file).read())
 
 	# Load time series dataset, and split it into train and test
-	X_train, y_train, X_test, y_test = data_helper.load_timeseries(train_file, params['window_size'], True)
+	x_train, y_train, x_test, y_test, x_test_raw, y_test_raw, last_window_raw, last_window = data_helper.load_timeseries(train_file, params['window_size'])
 
 	# Build RNN (LSTM) model
 	lstm_layer = [1, params['window_size'], params['hidden_unit'], 1]
@@ -24,22 +24,39 @@ def train_predict():
 
 	# Train RNN (LSTM) model with time series train set
 	model.fit(
-		X_train,
+		x_train,
 		y_train,
 		batch_size=params['batch_size'],
 		epochs=params['epochs'],
 		validation_split=params['validation_split'])
 
 	# Predict next time stamp 
-	predicted = build_model.predict_next_timestamp(model, X_test)        
+	predicted = build_model.predict_next_timestamp(model, x_test)        
 
-	print(type(predicted))
-	print(predicted.shape)
 	for i in range(len(predicted)):
 		print(predicted[i], y_test[i])
 
-	data_helper.plot_results(predicted, y_test)
+	print('raw')
+	predicted_raw = []
+	for i in range(len(x_test_raw)):
+		predicted_raw.append((predicted[i] + 1) * x_test_raw[i][0])
+		print(x_test_raw[i], y_test_raw[i])
+
+	data_helper.plot_results(predicted, y_test, predicted_raw, y_test_raw)
 	print('training is done')
+
+	print('last')
+	print(type(x_test))
+	print(x_test[0])
+	print(type(last_window))
+	print(last_window)
+	print(last_window_raw)
+
+	next_timestamp = build_model.predict_next_timestamp(model, last_window)
+	print(next_timestamp)
+	print(type(next_timestamp))
+	next_timestamp_raw = (next_timestamp[0] + 1) * last_window_raw[0][0]
+	print(next_timestamp_raw)
 
 if __name__ == '__main__':
 	# python3 train_predict.py ./data/sales.csv ./training_config.json_
